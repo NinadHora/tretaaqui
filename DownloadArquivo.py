d3eba400620473f7c37310ddefaadbe1324fcdfe
NinadHora <62748492+NinadHora@users.noreply.github.com>
2020-10-07 15:04:44 -0300

    arquivo down

---
+++ DownloadArquivo.py
@@ -0,0 +1,11 @@
+import requests 
+
+def baixar_arquivo(url, endereco):
+    resposta = requests.get(url, stream=True) #AQUI
+    if resposta.status_code == requests.codes.OK:
+        with open(endereco, 'wb') as novo_arquivo:
+                for parte in resposta.iter_content(chunk_size=256): #AQUI TBM
+                    novo_arquivo.write(parte)
+        print("Download finalizado. Arquivo salvo em: {}".format(endereco))
+    else:
+        resposta.raise_for_status()
